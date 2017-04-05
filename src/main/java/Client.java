6638a7859ad1fe8c65b1cefc296acc20a563172a
John Hunter <john@push-it-forward.com>
2017-04-05 10:48:26 -0700

    continued refactor

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,16 +1,33 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * Created by jhunter on 4/5/17.
  */
 public class Client {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        String fileString = "insight_testsuite/tests/test_features/log_input/log.txt";
 
-
-        Hosts hosts = new Hosts("insight_testsuite/tests/test_features/log_input/log.txt", 10);
+        Hosts hosts = new Hosts(10);
         Resources resources = new Resources("insight_testsuite/tests/test_features/log_input/log.txt", 10);
         Hours hours = new Hours("insight_testsuite/tests/test_features/log_input/log.txt", 10);
         Blocked blocked = new Blocked("insight_testsuite/tests/test_features/log_input/log.txt");
-    }
 
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileString))) {
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                hosts.processNextLine(line);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        hosts.outputResults("log_output/hosts.txt");
+    }
 }
--- src/main/java/Hosts.java
+++ src/main/java/Hosts.java
@@ -18,84 +18,72 @@ import java.util.*;
 */
 public class Hosts {
 
-    Map<String, HostNode> hostsMap;
-    Queue<HostNode> hostsQueue;
+    private Map<String, HostNode> hostsMap;
+    private Queue<HostNode> hostsQueue;
+    private int topX;
+
+    public Hosts(int topX) {
+        this.topX = topX;
+        hostsMap = new HashMap<>();
+        hostsQueue = new PriorityQueue<>(new Comparator<HostNode>() {
+            @Override
+            public int compare(HostNode hostNode, HostNode t1) {
+                if (hostNode.getValue() > t1.getValue()) {
+                    return 1;
+                }
+
+                if (hostNode.getValue() < t1.getValue()) {
+                    return -1;
+                }
+                return 0;
+            }
+        });
+    }
 
     public void processNextLine(String line) {
 
-    }
-
-    public void outputResults(String outputFile) {
-
-    }
-
-    public Hosts(String fileString, int topX) {
-
-        try (BufferedReader br = new BufferedReader(new FileReader(fileString))) {
-            hostsMap = new HashMap<>();
-            hostsQueue = new PriorityQueue<>(new Comparator<HostNode>() {
-                @Override
-                public int compare(HostNode hostNode, HostNode t1) {
-                    if (hostNode.getValue() > t1.getValue()) {
-                        return 1;
-                    }
-
-                    if (hostNode.getValue() < t1.getValue()) {
-                        return -1;
-                    }
-                    return 0;
-                }
-            });
-
-            String line;
-            String hostname;
-            while ((line = br.readLine()) != null) {
-                hostname = line.substring(0, line.indexOf(" "));
-                if (hostsMap.containsKey(hostname)) {
-                    hostsMap.get(hostname).incrementValue();
-                } else {
-                    hostsMap.put(hostname, new HostNode(hostname, 1));
-                }
-            }
-
-            hostsMap.forEach((k, v) -> {
-                if (hostsQueue.size() < topX) {
-                    hostsQueue.add(v);
-                } else {
-                    if (hostsQueue.peek().getValue() < v.getValue()) {
-                        hostsQueue.remove();
-                        hostsQueue.add(v);
-                    }
-                }
-            });
-
-            StringBuilder fileStringBuilder = new StringBuilder();
-
-            while (hostsQueue.size() > 0) {
-
-                HostNode smallest = hostsQueue.remove();
-                StringBuilder lineStringBuilder = new StringBuilder();
-
-                if (hostsQueue.size() > 0) {
-                    lineStringBuilder.append("\n");
-                }
-
-                lineStringBuilder.append(smallest.getHost());
-                lineStringBuilder.append(",");
-                lineStringBuilder.append(smallest.getValue());
-                fileStringBuilder.insert(0, lineStringBuilder);
-            }
-
-
-            BufferedWriter writer = new BufferedWriter(new FileWriter("log_output/hosts.txt"));
-            writer.append(fileStringBuilder);
-            writer.close();
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        String hostname;
+        hostname = line.substring(0, line.indexOf(" "));
+        if (hostsMap.containsKey(hostname)) {
+            hostsMap.get(hostname).incrementValue();
+        } else {
+            hostsMap.put(hostname, new HostNode(hostname, 1));
         }
+
+        hostsMap.forEach((k, v) -> {
+            if (hostsQueue.size() < topX) {
+                hostsQueue.add(v);
+            } else {
+                if (hostsQueue.peek().getValue() < v.getValue()) {
+                    hostsQueue.remove();
+                    hostsQueue.add(v);
+                }
+            }
+        });
     }
+
+    public void outputResults(String outputFile) throws IOException {
+
+        StringBuilder fileStringBuilder = new StringBuilder();
+
+        while (hostsQueue.size() > 0) {
+
+            HostNode smallest = hostsQueue.remove();
+            StringBuilder lineStringBuilder = new StringBuilder();
+
+            if (hostsQueue.size() > 0) {
+                lineStringBuilder.append("\n");
+            }
+
+            lineStringBuilder.append(smallest.getHost());
+            lineStringBuilder.append(",");
+            lineStringBuilder.append(smallest.getValue());
+            fileStringBuilder.insert(0, lineStringBuilder);
+        }
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
+        writer.append(fileStringBuilder);
+        writer.close();
+    }
+
 }
