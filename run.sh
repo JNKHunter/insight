32eb1e7e56832b602ec4f016a457a3a351a8ae01
John Hunter <john@push-it-forward.com>
2017-04-05 15:33:42 -0700

    changed run script to run main log file

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 javac -cp src/main/java/ -d bin src/main/java/Client.java
-java -cp bin Client ./insight_testsuite/tests/test_features/log_input/log.txt ./log_output/hosts.txt ./log_output/resources.txt ./log_output/hours.txt  ./log_output/blocked.txt
+java -cp bin Client ./log_input/log.txt ./log_output/hosts.txt ./log_output/resources.txt ./log_output/hours.txt  ./log_output/blocked.txt
