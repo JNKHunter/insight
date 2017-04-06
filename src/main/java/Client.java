6d610364ad5990334c9bf83b02b22bf86d8b3911
John Hunter <john@push-it-forward.com>
2017-04-05 19:11:01 -0700

    added shorter log file

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -16,13 +16,11 @@ public class Client {
         Resources resources = new Resources(10);
         Hours hours = new Hours(10);
         Blocked blocked = new Blocked();
-        int linenums = 0;
 
         try (BufferedReader br = new BufferedReader(new FileReader(fileString))) {
             String line;
 
             while ((line = br.readLine()) != null) {
-                linenums += 1;
                 hosts.processNextLine(line);
                 resources.processNextLine(line);
                 hours.processNextLine(line);
@@ -37,7 +35,5 @@ public class Client {
         resources.outputResults(args[2]);
         hours.outputResults(args[3]);
         blocked.outputResults(args[4]);
-
-        System.out.println("number of lines: " + linenums);
     }
 }
